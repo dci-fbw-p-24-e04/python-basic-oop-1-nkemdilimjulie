86a5b0f85247d707735325a4dd00e34e527a37f3
nkemdilim <nkemdilim.chime@dci-student.org>
2024-10-01 22:41:00 +0200

    calcuates the distance between two points

--- point.py
+++ point.py
@@ -1,45 +1,39 @@
 import math
 
 class Point:
-    # TODO: Replace 'pass' with your code
+   
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def move(self, x, y):
         self.x = x
         self.y = y
-        
-        #self.x, self.y = self.y, self.x
-        
-
+             
     def reset(self):
         #self.pointer = (0,0)
         self.x = 0
         self.y = 0
 
     def calc_distance(self, other):
-        #self = p1
-        #other = p2
+        ''' self = p1 --> holds the current object or instance; other = p2 --> holds the other  or 2nd instance'''
         x1 = self.x
         x2 = other.x
         y1 = self.y
         y2 = other.y
-       # self.distance = math.sqrt(((self.x - other.x)**2) + ((self.y - other.y)**2))
+        ''' calculates the square roots of the difference of the two points or two instances'''
         self.distance = math.sqrt(((x1- x2)**2) + ((y1- y2)**2))
         return self.distance
 
 
 if __name__ == "__main__":
     p1 = Point(3, 2)
-    p2 = Point(3, 7)
-    
-    
+    p2 = Point(3, 7)   
 
     print()
 
     p1.reset()
     p2.reset()
-
 
     p1.move(4, 7)
     p2.move(6, 2)
