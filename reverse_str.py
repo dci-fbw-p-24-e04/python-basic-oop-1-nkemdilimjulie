3cdb0f45e1ce8158c157aeb7331caef5bde1c89a
nkemdilim <nkemdilim.chime@dci-student.org>
2024-10-02 02:11:15 +0200

    reverses a string

--- reverse_str.py
+++ reverse_str.py
@@ -1,11 +1,14 @@
 from stack import Stack
 
 
-def reverse_string(my_string: str) -> str:
+def reverse_string(my_string):
+    
     rev_stack = Stack()
+    reversed = my_string[::-1]
+    return reversed
 
-    # TODO: your code goes here
-
+    
 
 if __name__ == "__main__":
+    
     print(reverse_string("hello"))  # olleh
